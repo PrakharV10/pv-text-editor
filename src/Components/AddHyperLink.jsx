67bd7c3868638211a03158f4454b2fe1de9b265e
Prakhar Varshney <70899048+PrakharV10@users.noreply.github.com>
2021-07-27 23:49:12 +0530

    Feat : Add And Remove Hyperlink

--- src/Components/AddHyperLink.jsx
+++ src/Components/AddHyperLink.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LinkIcon from '../Assets/Svg/LinkIcon';
+import UnlinkIcon from '../Assets/Svg/UnlinkIcon';
+import Modal from './Modal';
 
 function AddHyperLink({ editor }) {
+	const [openModal, setOpenModal] = useState(false);
+
+	function hyperlinkHandler(url) {
+		editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
+	}
+
 	return (
 		<>
 			<button
 				onClick={() => {
-					editor
-						.chain()
-						.focus()
-						.extendMarkRange('link')
-						.setLink({ href: 'prakharvarshney.tech' })
-						.run();
+					setOpenModal(true);
 				}}
 				className='mx-3'
 			>
@@ -19,6 +22,23 @@ function AddHyperLink({ editor }) {
 					<LinkIcon />
 				</span>
 			</button>
+			{editor.isActive('link') && (
+				<button
+					onClick={() => {
+						editor.chain().focus().unsetLink().run();
+					}}
+					className='mx-3'
+				>
+					<UnlinkIcon />
+				</button>
+			)}
+			{openModal && (
+				<Modal
+					modalType='Link'
+					setOpenModal={setOpenModal}
+					modalOutput={hyperlinkHandler}
+				/>
+			)}
 		</>
 	);
 }
